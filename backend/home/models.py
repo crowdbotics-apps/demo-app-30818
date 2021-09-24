7c00fcfc17abc28c169a9e765d3c61e2e380c966
Crowdbotics <team@crowdbotics.com>
2021-09-24 10:43:50 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,19 @@
+from django.conf import settings
 from django.db import models
+
+
+class Member(models.Model):
+    "Generated Model"
+    first_name = models.CharField(
+        max_length=256,
+    )
+    last_name = models.CharField(
+        max_length=256,
+    )
+    email = models.EmailField(
+        max_length=254,
+    )
+    phone_number = models.IntegerField()
+    gender = models.CharField(
+        max_length=256,
+    )
