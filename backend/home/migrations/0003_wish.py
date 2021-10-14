b63b0abfc5fdc4ad51ca5433abbc2f1a569a2609
Crowdbotics <team@crowdbotics.com>
2021-10-14 03:53:28 +0000

    Update models

--- backend/home/migrations/0003_wish.py
+++ backend/home/migrations/0003_wish.py
@@ -6,17 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_member'),
+        ("home", "0002_member"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Wish',
+            name="Wish",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=256)),
-                ('product_url', models.URLField()),
-                ('status', models.CharField(max_length=256)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=256)),
+                ("product_url", models.URLField()),
+                ("status", models.CharField(max_length=256)),
             ],
         ),
     ]
